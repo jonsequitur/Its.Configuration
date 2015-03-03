6a3544446fb78094e431ae5a434a3de60366a762
jonsequitur <jonsequeira@gmail.com>
2015-03-03 08:16:18 -0800

    remove MEF dependency and functionality, version bump

--- Configuration/Properties/AssemblyInfo.cs
+++ Configuration/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("Its.Configuration.Recipes")]
 [assembly: InternalsVisibleTo("Its.Configuration.Features")]
 
-[assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0-beta")]
